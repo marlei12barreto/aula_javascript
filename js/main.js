dbfb6ae81a3ffa155734bca3a49b60b333a78128
Marlei <marlei.barreto.silva@hotmail.com>
2020-11-20 20:16:18 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,100 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento")); 
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    //window,open("https://globallab.org/");
+    //window.location.href = "https://globallab.org/";
+
+}
+
+//var nome = " Marlei Barreto";
+//var idade = 43;
+//var frase = 0
+/*
+function soma (n1, n2){
+    return n1 + n2;
+}
+/*
+var validar = 0;
+
+function validadeIdade(idade){
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual é sua idade");
+validadeIdade(idade)
+console.log(validar);
+*/
+
+//function setReplace(frase, nome, novo_nome){
+    //return frase.replace(nome, novo_nome)
+//}
+
+//alert (soma(5, 10));
+//alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+//var d = new Date();
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getMinutes());
+
+
+//var idade = prompt("Qual é sua idade?");
+//var idade = 18;
+//if(idade >= 18){
+    //alert("maior de idade");
+//}else{
+    //alert("menor de idade");
+//};
+
+//var count;
+//for(count=0; count <= 5; count++){
+    //alert(count);
+//};
+
+
+
+
+//var count = 0;
+//while (count <= 5){
+    //console.log(count);
+    //alert(count);
+    //count = count + 1;
+//};
+
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+//alert("O melhor curso é na Digital Innovation One");
+
+var lista = ["maça", "pera", "laranja"];
+//console.log(lista);
+//lista.push("uva");
+//console.log(lista);
+//lista.pop("laranja");
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista);
+//console.log(lista.join(" - "));
+//console.log(lista.join(" | "));
+
+
+
+
+
+
